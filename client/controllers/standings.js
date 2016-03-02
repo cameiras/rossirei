5c980f1bf6ac4e653c4521b4466f1931ef5a24c3
Serena Cameirano <scameirano@codescience.com>
2016-03-02 09:16:38 -0800

    fix standings

--- client/controllers/standings.js
+++ client/controllers/standings.js
@@ -2,9 +2,7 @@ app.controller('standingsCtrl', function($scope, $rootScope, $http, $window) {
 
    $http.post('/style/allStandings').
         success(function(data) {
-        	console.log('DATA' + data);
-            $scope.standings = data.sort(function(a,b) { return b.liked.length - a.liked.length });
-            console.log(data[1]);
+        	$scope.standings = data.sort(function(a,b) { return b.liked.length - a.liked.length });
         }).error(function(data) {
             console.error("error in posting");
         });
